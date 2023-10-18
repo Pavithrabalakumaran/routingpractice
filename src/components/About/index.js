b800860e5ccb2e6842935f7918fe04379cbfd195
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2023-10-18 14:30:54 +0000

    rp2

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+// Write your JS code here
+import './index.css'
+
+const About = () => {
+  ;<div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      className="about-image"
+      alt="about"
+    />
+    <h1 className="heading">About</h1>
+    <p className="para-des">I love to create! I am a frontend Developer</p>
+  </div>
+}
+
+export default About
---
+++ src/components/Contact/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+// Write your JS code here
+import './index.css'
+
+const Contact = () => {
+  ;<div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      className="about-image"
+      alt="about"
+    />
+    <h1 className="heading">Contact</h1>
+  </div>
+}
+
+export default Contact
